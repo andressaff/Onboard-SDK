33bbbda0aafa92515854e969fefa8c238abb28ba
andressaff <andressa.silva@autaza.com>
2022-03-29 11:14:31 -0300

    sdk

---
+++ Onboard-SDK-ROS/src/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dji/catkin_ws/src/Onboard-SDK-ROS/src
+# Build directory: /home/dji/catkin_ws/src/Onboard-SDK/Onboard-SDK-ROS/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(dji_osdk_ros)
+subdirs(dji_osdk_ros_obsoleted)
